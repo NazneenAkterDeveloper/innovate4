45008b5d616b39ca088e292b42ba3ed2264c5e43
Nazneen Akter <nazneenzaman195@gmail.com>
2025-01-16 22:01:16 +0600

    Add files via upload

---
+++ js/tailwind.config.js
@@ -0,0 +1,43 @@
+tailwind.config = {
+    theme: {
+      extend: {
+
+        // color 
+        colors: {
+         headerBg: '#F5F5FF',
+         btnColor:'#7258F7',
+         bgColor:'#FCF8F8',
+         h1color:'#29313D',
+         cardColor:'#E2DDFB',
+         grayColor:'#6C7D93',
+         yellow:'#FFC556',
+         navy:'#181059',
+         lycolor:'#FEF8E4',
+         pcolor:'#29313D',
+         box:'#DBCCFC',
+         gcolor:'#545A64',
+         blue:'#0D87E7',
+         purple:'#6078FF',
+         red:'#F27860',
+         green:'#74BA52',
+        },
+        // color 
+        // container 
+        maxWidth: {
+        'headerContainer': '1170px',
+      },
+    //   container 
+    // font family 
+    fontFamily:{
+        'open':["Open Sans"],
+        'papri':["Paprika"],
+    },
+    // font family 
+    Background:{
+        'bannerBg':"url('../images/gradi.png')",
+     
+
+    }
+      }
+    }
+  }
